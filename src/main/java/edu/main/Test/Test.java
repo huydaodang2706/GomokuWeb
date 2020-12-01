e0041201c150009905806ec46d64a4c1f05a84e8
huydaodang2706 <huydaodang2706@gmail.com>
2020-12-01 20:05:11 +0700

    Add gson dependency to prj

---
+++ src/main/java/edu/main/Test/Test.java
@@ -0,0 +1,51 @@
+package edu.main.Test;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class Test{
+    public static void main(String[] args) {
+        String json ="{"
+                + "\"Emp_Name\":\"Robert\""
+                + ",\"Emp_Gen\":\"No1332\""
+                + ",\"Emp_dept\":\"Developement\""
+                + ",\"Emp_age\":\"25\""
+                + ",\"Emp_Pos\":\"Staff\""
+                + ",\"Emp_colleague\":[\"Lina\",\"Tom\",\"Thomas\"]"
+                + "}";
+        // Cách 1 chuyển đổi json thành object
+        Gson gson = new Gson(); // khởi tạo Gson
+        Employee employee = gson.fromJson(json, Employee.class); // parse Gson về object
+        System.out.println("Emp_Name:"+ employee.emp_Name);
+        System.out.println("Emp_Gen:"+ employee.emp_Gen);
+        System.out.println("Emp_age:"+ employee.emp_age);
+        System.out.println("Emp_Pos:"+ employee.emp_Pos);
+        System.out.println("Emp_dept:"+ employee.emp_dept);
+        System.out.println("Emp_colleague:"+ employee.emp_colleague.toString());
+//        // cách 2: không chuyển đổi json thành object mà parse trực tiếp
+//       JsonObject jsonObject =JsonParser.parseString(json).getAsJsonObject();// Nếu bạn không muốn chuyển thành object có thể sử dụng
+//       JsonArray jsonArray = (JsonArray) jsonObject.get("Emp_colleague");
+//       System.out.println("Emp_Name:"+ jsonObject.get("Emp_Name"));
+//       System.out.println("Emp_Gen:"+ jsonObject.get("Emp_Gen"));
+//       System.out.println("Emp_age:"+ jsonObject.get("Emp_age"));
+//       System.out.println("Emp_Pos:"+ jsonObject.get("Emp_Pos"));
+//       System.out.println("Emp_dept:"+ jsonObject.get("Emp_dept"));
+//       System.out.println("Emp_colleague:"+ jsonObject.get("Emp_colleague").toString());
+
+    }
+    public class Employee
+    {
+        @SerializedName("Emp_Name") // SerializedName giống với Key của Json
+        public String emp_Name;
+        @SerializedName("Emp_Gen")
+        public String emp_Gen;
+        @SerializedName("Emp_dept")
+        public String emp_dept;
+        @SerializedName("Emp_age")
+        public String emp_age;
+        @SerializedName("Emp_Pos")
+        public String emp_Pos;
+        @SerializedName("Emp_colleague")
+        public List<String> emp_colleague;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module edu.hedspi {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+    requires gson;
 
-    opens edu.main to javafx.fxml;
+    opens edu.main to javafx.fxml,gson;
     exports edu.main;
 }
