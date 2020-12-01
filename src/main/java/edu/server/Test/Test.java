46368813d93e054f6b984fc1861c43c69ba2b87b
huydaodang2706 <huydaodang2706@gmail.com>
2020-12-01 23:09:29 +0700

    Install gson and json.simple

--- src/main/java/edu/server/Test/Test.java
+++ src/main/java/edu/server/Test/Test.java
@@ -1,4 +1,4 @@
-package edu.main.Test;
+package edu.server.Test;
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
@@ -33,19 +33,5 @@ public class Test{
 //       System.out.println("Emp_colleague:"+ jsonObject.get("Emp_colleague").toString());
 
     }
-    public class Employee
-    {
-        @SerializedName("Emp_Name") // SerializedName giống với Key của Json
-        public String emp_Name;
-        @SerializedName("Emp_Gen")
-        public String emp_Gen;
-        @SerializedName("Emp_dept")
-        public String emp_dept;
-        @SerializedName("Emp_age")
-        public String emp_age;
-        @SerializedName("Emp_Pos")
-        public String emp_Pos;
-        @SerializedName("Emp_colleague")
-        public List<String> emp_colleague;
-    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ module edu.hedspi {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
-    requires gson;
-
+    requires com.google.gson;
     requires json.simple;
-    opens edu.main to javafx.fxml, gson, json.simple;
+    opens edu.main to javafx.fxml, com.google.gsonc, json.simple;
+    opens edu.server.Test to com.google.gson;
+    exports edu.server.Test;
     exports edu.main;
 }
